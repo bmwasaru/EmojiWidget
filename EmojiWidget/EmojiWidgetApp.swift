4bbc3653a85dd5e3b30c531fb9614b064a1b5dc3
britone mwasaru <bmwasaru@gmail.com>
2021-01-16 09:52:15 +0300

    Initial Commit

---
+++ EmojiWidget/EmojiWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiWidgetApp.swift
+//  EmojiWidget
+//
+//  Created by Britone Mwasaru on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
